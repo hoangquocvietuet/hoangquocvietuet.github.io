be0d776cb92b8b6125ea55ed9dabed3d937d7e21
hoangquocvietuet@gmail.com <hoangquocvietuet@gmail.com>
2023-11-08 17:08:01 +0700

    configured site

--- posts/polygon-codeforces-tutorial/code-fragments/example-scripts/gen-totally-random-example.sh
+++
@@ -1,1 +0,0 @@
-./gen-totally-random -test-count 5 -sum-n 20 -min-a 1 -max-a 5
--- posts/polygon-codeforces-tutorial/code-fragments/example-scripts/gen-v2-example1.sh
+++
@@ -1,3 +0,0 @@
-./gen-v2 -test-count 3 -sum-n 40 -min-n 10 -min-a 1 -max-a 100 -min-b 1 -max-b 100 > test.txt
-cat test.txt
-./solution < test.txt
